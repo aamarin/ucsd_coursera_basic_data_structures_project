452fcc6bc726e4c330616838fce3a9b171b652ab
Andre Marin <andre@alexamarin.com>
2020-01-03 21:00:13 -0600

    Added unit tests for bracket checking algorithm

--- projects/week1_basic_data_structures/1_brackets_in_code/check_brackets.cpp
+++ projects/week1_basic_data_structures/1_brackets_in_code/check_brackets.cpp
@@ -3,22 +3,27 @@
 
 std::string check_brackets(const std::string& text) {
     std::stack <Bracket> opening_brackets_stack;
+
     for (int position = 0; position < text.length(); ++position) {
         char next = text[position];
-        Bracket l_bracket(next, position);
+        Bracket l_bracket(next, position + 1);
 
         if (next == '(' || next == '[' || next == '{') {
-            // Process opening bracket, write your code here
+            // Process opening bracket
+            if(text.length() == 1) return std::to_string(position + 1);
             opening_brackets_stack.push(l_bracket);
         }
 
-        if (next == ')' || next == ']' || next == '}') {
-            // Process closing bracket, write your code here
+        else {
+            // Process closing bracket
+            if(opening_brackets_stack.empty()) return std::to_string(position + 1);
+
             auto top = opening_brackets_stack.top();
-            if (!top.Matchc(next)) {
-                return std::to_string(top.position);
-            }
-        }
+            opening_brackets_stack.pop();
+
+            if( !top.Matchc(next) ) return std::to_string(top.position + 1);
+        }// else
     }// position
-    return "Success";
+
+    return (std::string("Success"));
 }
